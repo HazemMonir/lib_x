eb0b38b40077beb8a6e576eb7aec5a827c7e7fb1
HazemMonir <hazem.monir@gmail.com>
2022-12-03 21:02:42 +0200

    fixed exports

--- lib/lib_x.dart
+++ lib/lib_x.dart
@@ -9,8 +9,6 @@ export 'package:flutter/foundation.dart';
 export 'package:flutter/material.dart';
 export 'package:flutter/rendering.dart';
 export 'package:flutter/services.dart';
-export 'package:flutter_svg/flutter_svg.dart';
-export 'package:flutter_svg/svg.dart';
 export 'package:font_awesome_flutter/font_awesome_flutter.dart';
 export 'package:google_fonts/google_fonts.dart';
 //
@@ -36,8 +34,6 @@ export 'package:lib_x/src/x/widgets/_misc.dart';
 export 'package:lib_x/src/x/widgets/dismissable_modal.dart';
 export 'package:lib_x/src/x/widgets/persist_state_widget.dart';
 export 'package:lib_x/src/x/widgets/scaffold_x.dart';
-export 'package:mime/mime.dart';
 export 'package:overlay_support/overlay_support.dart';
 export 'package:routemaster/routemaster.dart';
-export 'package:shimmer/shimmer.dart';
 export 'package:url_strategy/url_strategy.dart';
